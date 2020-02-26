7eb8e9fba8d397eb8e83b1ec6c18a91c1e59cdc7
Hannes Molsen <mail@hannesmolsen.de>
2020-02-26 23:35:11 +0100

    i18n + english translation for xss

--- webapps/xss/evil.js
+++ webapps/xss/evil.js
@@ -1,6 +1,13 @@
 setTimeout(function(){
-	// Cookies auslesen und in Variable speichern
-	// URL erzeugen, die Cookie-Inhalt an den Logger schickt
-	// XMLHttpRequest erstellen
-	// XMLHttpRequest als GET an die URL senden
+	// DE: Cookies auslesen und in Variable speichern
+	// EN: Read out cookies and save them in a variable
+	
+	// DE: URL erzeugen, die Cookie-Inhalt an den Logger schickt
+	// EN: Concatenate a URL that is able to send the cookie content to the logger
+	
+	// DE: XMLHttpRequest erstellen
+	// EN: create an XMLHttpRequest
+	
+	// DE: XMLHttpRequest als GET an die URL senden
+	// EN: send the XMLHttpRequest as GET request to the URL from above
 }, 500);
