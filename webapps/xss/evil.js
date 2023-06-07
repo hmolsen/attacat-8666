b1b9ab5d465ac2041a997daa256487a0b0889786
Hannes Molsen <mail@hannesmolsen.de>
2023-06-07 16:20:00 +0200

    evil.js changed from XMLHttpRequest to fetch-API

--- webapps/xss/evil.js
+++ webapps/xss/evil.js
@@ -2,12 +2,9 @@ setTimeout(function(){
 	// DE: Cookies auslesen und in Variable speichern
 	// EN: Read out cookies and save them in a variable
 	
-	// DE: URL erzeugen, die Cookie-Inhalt an den Logger schickt
+	// DE: URL erzeugen, die Cookie-Inhalt per Query-Parameter an den Logger schickt
 	// EN: Concatenate a URL that is able to send the cookie content to the logger
 	
-	// DE: XMLHttpRequest erstellen
-	// EN: create an XMLHttpRequest
-	
-	// DE: XMLHttpRequest als GET an die URL senden
-	// EN: send the XMLHttpRequest as GET request to the URL from above
+	// DE: fetch-API verwenden, um GET-Request an logger zu schicken
+	// EN: use the fetch-API to send a GET-Request to the logger
 }, 500);
